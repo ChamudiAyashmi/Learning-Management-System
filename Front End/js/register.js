bf4f86efb8e78b760454b83ede37e9a34a4b29e0
ChamudiAyashmi <chamudi2003@gmail.com>
2023-08-30 11:47:56 +0530

    update

---
+++ Front End/js/register.js
@@ -0,0 +1,34 @@
+const firstName = document.getElementById("txtFName");
+const lastName = document.getElementById("txtLName");
+const userName = document.getElementById("txtUserName");
+const password = document.getElementById("txtPassword");
+const email = document.getElementById("txtEmail");
+const age = document.getElementById("txtAge");
+const address = document.getElementById("txtAddress");
+const phoneNumber = document.getElementById("txtPhone");
+const gender = document.getElementsByName("gender");
+const btnRegister = document.getElementById("buttonRegister");
+
+btnRegister.addEventListener("click",()=>{
+    let selectGender;
+    for(i =0; i<gender.length; i++){
+        if(gender[i].checked){
+            selectGender=gender[i].value;
+        }
+    }
+
+    let student = {
+        "firstName" : firstName.value,
+        "lastName" : lastName.value,
+        "userName" : userName.value,
+        "password" : password.value,
+        "emailAddress" : email.value,
+        "age" : age.value,
+        "address" : address.value,
+        "phoneNumber" : phoneNumber.value,
+        "gender" : selectGender
+    }
+
+    fetch('http://localhost:8080/student')
+
+})
